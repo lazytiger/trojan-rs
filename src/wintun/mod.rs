c04100c08a4092d80954720e12052755f6c149af
hoping <baihaoping@gmail.com>
2021-12-24 15:41:14 +0800

    extract function from run

--- src/wintun/mod.rs
+++ src/wintun/mod.rs
@@ -1,6 +1,6 @@
 use std::{collections::BTreeMap, convert::TryInto, process::Command, sync::Arc, thread};
 
-use crossbeam::channel::Sender;
+use crossbeam::channel::{Receiver, Sender};
 use mio::{Events, Poll, Token, Waker};
 use rustls::{ClientConfig, OwnedTrustAnchor, RootCertStore};
 use smoltcp::{
@@ -54,81 +54,84 @@ const CHANNEL_UDP: usize = 1;
 /// Channel index for remote tcp connection
 const CHANNEL_TCP: usize = 2;
 
-pub fn run() -> Result<()> {
-    log::info!("dll:{}", OPTIONS.wintun_args().wintun);
-    let wintun = unsafe { wintun::load_from_path(&OPTIONS.wintun_args().wintun)? };
-    let adapter = Adapter::create(&wintun, "trojan", OPTIONS.wintun_args().name.as_str(), None)?;
-    let session = Arc::new(adapter.start_session(wintun::MAX_RING_CAPACITY)?);
-    let index = adapter.get_adapter_index()?;
-
-    if let Some(file) = &OPTIONS.wintun_args().route_ipset {
-        let mut ipset = IPSet::with_file(file.as_str())?;
-        if OPTIONS.wintun_args().inverse_route {
-            ipset = !ipset;
-        }
-        //route_add_with_if(757147431, !0, index)?;
-        //route_add_with_if(794538172, !0, index)?;
-        ipset.add_route(index)?;
-        log::warn!("route add completed");
-    }
-
-    while get_adapter_ip(OPTIONS.wintun_args().name.as_str()).is_none() {
-        thread::sleep(std::time::Duration::new(1, 0));
-    }
-    let gateway = get_adapter_ip(OPTIONS.wintun_args().name.as_str()).unwrap();
-    log::warn!("wintun is ready at:{}", gateway);
-
-    if OPTIONS.wintun_args().with_dns {
-        let _ = thread::spawn(|| {
-            let program = std::env::current_exe().unwrap();
-            let args = OPTIONS.wintun_args();
-            let log_file = if !OPTIONS.log_file.is_empty() {
-                OPTIONS.log_file.clone() + ".dns"
-            } else {
-                "".into()
-            };
-            let message = match Command::new(program.clone())
-                .args([
-                    "--log-file",
-                    log_file.as_str(),
-                    "--local-addr",
-                    OPTIONS.local_addr.as_str(),
-                    "--password",
-                    OPTIONS.password.as_str(),
-                    "--log-level",
-                    OPTIONS.log_level.to_string().as_str(),
-                    "--udp-idle-timeout",
-                    OPTIONS.udp_idle_timeout.to_string().as_str(),
-                    "--tcp-idle-timeout",
-                    OPTIONS.tcp_idle_timeout.to_string().as_str(),
-                    "dns",
-                    "-n",
-                    args.name.as_str(),
-                    "--blocked-domain-list",
-                    args.blocked_domain_list.as_str(),
-                    "--dns-listen-address",
-                    args.dns_listen_address.as_str(),
-                    "--trusted-dns",
-                    args.trusted_dns.as_str(),
-                    "--poisoned-dns",
-                    args.poisoned_dns.as_str(),
-                ])
-                .output()
-            {
-                Ok(output) => {
-                    if output.status.success() {
-                        "success".into()
-                    } else {
-                        String::from_utf8(output.stderr).unwrap()
-                    }
+fn start_dns() {
+    let _ = thread::spawn(|| {
+        let program = std::env::current_exe().unwrap();
+        let args = OPTIONS.wintun_args();
+        let log_file = if !OPTIONS.log_file.is_empty() {
+            OPTIONS.log_file.clone() + ".dns"
+        } else {
+            "".into()
+        };
+        let message = match Command::new(program.clone())
+            .args([
+                "--log-file",
+                log_file.as_str(),
+                "--local-addr",
+                OPTIONS.local_addr.as_str(),
+                "--password",
+                OPTIONS.password.as_str(),
+                "--log-level",
+                OPTIONS.log_level.to_string().as_str(),
+                "--udp-idle-timeout",
+                OPTIONS.udp_idle_timeout.to_string().as_str(),
+                "--tcp-idle-timeout",
+                OPTIONS.tcp_idle_timeout.to_string().as_str(),
+                "dns",
+                "-n",
+                args.name.as_str(),
+                "--blocked-domain-list",
+                args.blocked_domain_list.as_str(),
+                "--dns-listen-address",
+                args.dns_listen_address.as_str(),
+                "--trusted-dns",
+                args.trusted_dns.as_str(),
+                "--poisoned-dns",
+                args.poisoned_dns.as_str(),
+            ])
+            .output()
+        {
+            Ok(output) => {
+                if output.status.success() {
+                    "success".into()
+                } else {
+                    String::from_utf8(output.stderr).unwrap()
                 }
-                Err(err) => err.to_string(),
-            };
-            log::error!("trojan dns exit with message:{}", message);
-            std::process::exit(-1);
-        });
-    }
+            }
+            Err(err) => err.to_string(),
+        };
+        log::error!("trojan dns exit with message:{}", message);
+        std::process::exit(-1);
+    });
+}
 
+fn start_device_send(tx_receiver: Receiver<Vec<u8>>, tx_session: Arc<Session>) {
+    let _ = thread::spawn(move || {
+        while let Ok(data) = tx_receiver.recv() {
+            match tx_session.allocate_send_packet(data.len() as u16) {
+                Ok(mut packet) => {
+                    packet.bytes_mut().copy_from_slice(data.as_slice());
+                    tx_session.send_packet(packet);
+                }
+                Err(err) => {
+                    log::error!("allocate send packet failed:{:?}", err);
+                }
+            }
+        }
+    });
+}
+
+fn apply_ipset(file: &str, index: u32) -> Result<()> {
+    let mut ipset = IPSet::with_file(file)?;
+    if OPTIONS.wintun_args().inverse_route {
+        ipset = !ipset;
+    }
+    ipset.add_route(index)?;
+    log::warn!("route add completed");
+    Ok(())
+}
+
+fn prepare_idle_pool(poll: &Poll, resolver: &DnsResolver) -> Result<IdlePool> {
     let hostname = OPTIONS.wintun_args().hostname.as_str().try_into()?;
     let mut root_store = RootCertStore::empty();
     root_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.0.iter().map(|ta| {
@@ -146,10 +149,6 @@ pub fn run() -> Result<()> {
         .with_root_certificates(root_store)
         .with_no_client_auth();
     let config = Arc::new(config);
-
-    let mut poll = Poll::new()?;
-    let waker = Arc::new(Waker::new(poll.registry(), Token(RESOLVER))?);
-    let mut resolver = DnsResolver::new(waker, Token(RESOLVER));
     let mut pool = IdlePool::new(
         config,
         hostname,
@@ -157,19 +156,22 @@ pub fn run() -> Result<()> {
         OPTIONS.wintun_args().port,
         OPTIONS.wintun_args().hostname.clone(),
     );
-    pool.init(&poll, &resolver);
+    pool.init(poll, resolver);
     pool.init_index(CHANNEL_CNT, CHANNEL_IDLE, MIN_INDEX, MAX_INDEX);
+    Ok(pool)
+}
 
-    let (rx_sender, rx_receiver) = crossbeam::channel::bounded(OPTIONS.wintun_args().buffer_size);
-    let (tx_sender, tx_receiver) = crossbeam::channel::bounded(OPTIONS.wintun_args().buffer_size);
-
+fn prepare_device<'a>(
+    tx_sender: Sender<Vec<u8>>,
+    rx_receiver: Receiver<Vec<u8>>,
+) -> Interface<'a, TunInterface> {
     let ip_addrs = [IpCidr::new(IpAddress::v4(0, 0, 0, 1), 0)];
 
     let mut routes = Routes::new(BTreeMap::new());
     routes
         .add_default_ipv4_route(Ipv4Address::new(0, 0, 0, 1))
         .unwrap();
-    let mut interface = InterfaceBuilder::new(
+    let interface = InterfaceBuilder::new(
         TunInterface::new(tx_sender, rx_receiver, OPTIONS.wintun_args().mtu),
         [],
     )
@@ -177,100 +179,7 @@ pub fn run() -> Result<()> {
     .ip_addrs(ip_addrs)
     .routes(routes)
     .finalize();
-
-    let mut events = Events::with_capacity(1024);
-    let timeout = Some(Duration::from_millis(1));
-    let mut udp_server = UdpServer::new();
-    let mut tcp_server = TcpServer::new();
-
-    let mut last_udp_check_time = std::time::Instant::now();
-    let mut last_tcp_check_time = std::time::Instant::now();
-    let check_duration = std::time::Duration::new(10, 0);
-
-    let tx_session = session.clone();
-    let _ = thread::spawn(move || {
-        while let Ok(data) = tx_receiver.recv() {
-            match tx_session.allocate_send_packet(data.len() as u16) {
-                Ok(mut packet) => {
-                    packet.bytes_mut().copy_from_slice(data.as_slice());
-                    tx_session.send_packet(packet);
-                }
-                Err(err) => {
-                    log::error!("allocate send packet failed:{:?}", err);
-                }
-            }
-        }
-    });
-
-    let mut now = Instant::now();
-    let mut wakers = Wakers::new();
-    loop {
-        wakers.clear();
-        do_tun_read(
-            &session,
-            &rx_sender,
-            &mut interface,
-            &udp_server,
-            &mut wakers,
-        )?;
-        if let Err(err) = interface.poll(now) {
-            log::info!("interface error:{}", err);
-        }
-        udp_server.do_local(&mut pool, &poll, &resolver, &mut wakers, &mut interface);
-        tcp_server.do_local(&mut pool, &poll, &resolver, &mut wakers, &mut interface);
-
-        now = Instant::now();
-        let timeout = interface.poll_delay(now).or(timeout);
-        poll.poll(
-            &mut events,
-            timeout.map(|d| std::time::Duration::from_millis(d.total_millis())),
-        )?;
-        for event in &events {
-            match event.token().0 {
-                RESOLVER => {
-                    resolver.consume(|_, ip| {
-                        pool.resolve(ip);
-                    });
-                }
-                i if i % CHANNEL_CNT == CHANNEL_IDLE => {
-                    pool.ready(event, &poll);
-                }
-                i if i % CHANNEL_CNT == CHANNEL_UDP => {
-                    udp_server.do_remote(event, &poll, &mut interface, &mut wakers);
-                }
-                _ => {
-                    tcp_server.do_remote(event, &poll, &mut interface, &mut wakers);
-                }
-            }
-        }
-
-        let now = std::time::Instant::now();
-        if now - last_tcp_check_time > check_duration {
-            tcp_server.check_timeout(&poll, now, &mut interface);
-            last_tcp_check_time = now;
-            let sockets_count = interface.sockets().fold(0, |count, (_, socket)| {
-                if matches!(socket, Socket::Tcp(_)) {
-                    count + 1
-                } else {
-                    count
-                }
-            });
-            log::warn!("total tcp sockets count:{}", sockets_count);
-        }
-
-        if now - last_udp_check_time > OPTIONS.udp_idle_duration {
-            udp_server.check_timeout(now, &mut interface);
-            last_udp_check_time = now;
-            let sockets_count = interface.sockets().fold(0, |count, (_, socket)| {
-                if matches!(socket, Socket::Udp(_)) {
-                    count + 1
-                } else {
-                    count
-                }
-            });
-            log::warn!("total udp sockets count:{}", sockets_count);
-        }
-    }
+    interface
 }
 
 fn do_tun_read(
@@ -367,4 +276,115 @@ fn do_tun_read(
         }
     }
     Ok(())
+}
+
+pub fn run() -> Result<()> {
+    log::info!("dll:{}", OPTIONS.wintun_args().wintun);
+    let wintun = unsafe { wintun::load_from_path(&OPTIONS.wintun_args().wintun)? };
+    let adapter = Adapter::create(&wintun, "trojan", OPTIONS.wintun_args().name.as_str(), None)?;
+    let session = Arc::new(adapter.start_session(wintun::MAX_RING_CAPACITY)?);
+    let index = adapter.get_adapter_index()?;
+
+    if let Some(file) = &OPTIONS.wintun_args().route_ipset {
+        apply_ipset(file, index)?;
+    }
+
+    let mut poll = Poll::new()?;
+    let waker = Arc::new(Waker::new(poll.registry(), Token(RESOLVER))?);
+    let mut resolver = DnsResolver::new(waker, Token(RESOLVER));
+    let mut pool = prepare_idle_pool(&poll, &resolver)?;
+
+    let mut udp_server = UdpServer::new();
+    let mut tcp_server = TcpServer::new();
+
+    let (rx_sender, rx_receiver) = crossbeam::channel::bounded(OPTIONS.wintun_args().buffer_size);
+    let (tx_sender, tx_receiver) = crossbeam::channel::bounded(OPTIONS.wintun_args().buffer_size);
+    let mut interface = prepare_device(tx_sender, rx_receiver);
+
+    start_device_send(tx_receiver, session.clone());
+
+    while get_adapter_ip(OPTIONS.wintun_args().name.as_str()).is_none() {
+        thread::sleep(std::time::Duration::new(1, 0));
+    }
+    let gateway = get_adapter_ip(OPTIONS.wintun_args().name.as_str()).unwrap();
+    log::warn!("wintun is ready at:{}", gateway);
+
+    if OPTIONS.wintun_args().with_dns {
+        start_dns();
+    }
+
+    let mut events = Events::with_capacity(1024);
+    let timeout = Some(Duration::from_millis(1));
+    let mut last_udp_check_time = std::time::Instant::now();
+    let mut last_tcp_check_time = std::time::Instant::now();
+    let check_duration = std::time::Duration::new(10, 0);
+    let mut now = Instant::now();
+    let mut wakers = Wakers::new();
+    loop {
+        wakers.clear();
+        do_tun_read(
+            &session,
+            &rx_sender,
+            &mut interface,
+            &udp_server,
+            &mut wakers,
+        )?;
+        if let Err(err) = interface.poll(now) {
+            log::info!("interface error:{}", err);
+        }
+        udp_server.do_local(&mut pool, &poll, &resolver, &mut wakers, &mut interface);
+        tcp_server.do_local(&mut pool, &poll, &resolver, &mut wakers, &mut interface);
+
+        now = Instant::now();
+        let timeout = interface.poll_delay(now).or(timeout);
+        poll.poll(
+            &mut events,
+            timeout.map(|d| std::time::Duration::from_millis(d.total_millis())),
+        )?;
+        for event in &events {
+            match event.token().0 {
+                RESOLVER => {
+                    resolver.consume(|_, ip| {
+                        pool.resolve(ip);
+                    });
+                }
+                i if i % CHANNEL_CNT == CHANNEL_IDLE => {
+                    pool.ready(event, &poll);
+                }
+                i if i % CHANNEL_CNT == CHANNEL_UDP => {
+                    udp_server.do_remote(event, &poll, &mut interface, &mut wakers);
+                }
+                _ => {
+                    tcp_server.do_remote(event, &poll, &mut interface, &mut wakers);
+                }
+            }
+        }
+
+        let now = std::time::Instant::now();
+        if now - last_tcp_check_time > check_duration {
+            tcp_server.check_timeout(&poll, now, &mut interface);
+            last_tcp_check_time = now;
+            let sockets_count = interface.sockets().fold(0, |count, (_, socket)| {
+                if matches!(socket, Socket::Tcp(_)) {
+                    count + 1
+                } else {
+                    count
+                }
+            });
+            log::warn!("total tcp sockets count:{}", sockets_count);
+        }
+
+        if now - last_udp_check_time > OPTIONS.udp_idle_duration {
+            udp_server.check_timeout(now, &mut interface);
+            last_udp_check_time = now;
+            let sockets_count = interface.sockets().fold(0, |count, (_, socket)| {
+                if matches!(socket, Socket::Udp(_)) {
+                    count + 1
+                } else {
+                    count
+                }
+            });
+            log::warn!("total udp sockets count:{}", sockets_count);
+        }
+    }
 }
