906f0fc92ba2f2d929307252a9572412aec2f625
hoping <hoping@babeltime.com>
2023-02-07 18:44:59 +0800

    fix first time crash

--- src/config.rs
+++ src/config.rs
@@ -364,15 +364,18 @@ impl Opts {
 }
 
 pub fn setup_logger(logfile: &str, level: u8) -> crate::types::Result<()> {
-    let mut suffix = 1;
-    loop {
-        let new_file = logfile.to_string() + "." + suffix.to_string().as_str();
-        let path = Path::new(new_file.as_str());
-        if !path.exists() {
-            std::fs::rename(logfile, new_file.as_str())?;
-            break;
-        } else {
-            suffix += 1;
+    let path = Path::new(logfile);
+    if path.exists() {
+        let mut suffix = 1;
+        loop {
+            let new_file = logfile.to_string() + "." + suffix.to_string().as_str();
+            let path = Path::new(new_file.as_str());
+            if !path.exists() {
+                std::fs::rename(logfile, new_file.as_str())?;
+                break;
+            } else {
+                suffix += 1;
+            }
         }
     }
     let level = match level {
