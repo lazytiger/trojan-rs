eabc781c0581928d9c4ff66834af6a81378d89e5
hoping <baihaoping@gmail.com>
2023-02-22 10:58:04 +0800

    readonly when running

--- trojan-client/src/app.vue
+++ trojan-client/src/app.vue
@@ -24,6 +24,7 @@ export default {
         trust_dns: "",
       },
       label: "开始",
+      running: false,
     }
   },
   methods: {
@@ -56,13 +57,15 @@ export default {
         await info("event:state-update, label:" + event.windowLabel + ", payload:" + event.payload);
         if (event.payload) {
           this.label = "停止";
+          this.running = true;
         } else {
           this.label = "开始";
+          this.running = false;
         }
       });
     },
     do_action() {
-      if (this.label === "开始") {
+      if (!this.running) {
         this.start();
       } else {
         this.stop();
@@ -82,22 +85,25 @@ export default {
   <v-app>
     <v-main class="bg-grey-lighten-4">
       <v-container class="mx-auto" style="max-width: 480px;">
-        <v-text-field v-model="config.iface_name" label="虚拟网卡名" variant="outlined"></v-text-field>
-        <v-text-field v-model="config.server_domain" label="服务器域名" variant="outlined"></v-text-field>
-        <v-text-field v-model="config.server_auth" :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-                      :type="show ? 'text' : 'password'" label="服务器密码" variant="outlined"
-                      @click:append="show = !show"></v-text-field>
-        <v-text-field v-model="config.default_dns" :rules="[check_ipv4]" label="默认DNS"
+        <v-text-field v-model="config.iface_name" :readonly="running" label="虚拟网卡名"
                       variant="outlined"></v-text-field>
+        <v-text-field v-model="config.server_domain" :readonly="running" label="服务器域名"
+                      variant="outlined"></v-text-field>
+        <v-text-field v-model="config.server_auth" :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+                      :readonly="running" :type="show ? 'text' : 'password'" label="服务器密码"
+                      variant="outlined" @click:append="show = !show"></v-text-field>
+        <v-text-field v-model="config.default_dns" :readonly="running" :rules="[check_ipv4]"
+                      label="默认DNS" variant="outlined"></v-text-field>
         <v-combobox v-model="config.log_level"
                     :items="['Trace', 'Debug', 'Info', 'Warn', 'Error', 'Off']"
-                    label="日志级别"
-                    variant="solo"
+                    :readonly="running"
+                    label="日志级别" variant="solo"
         ></v-combobox>
-        <v-slider v-model="config.pool_size" label="连接池大小" max="20" min="0" step="1">
+        <v-slider v-model="config.pool_size" :readonly="running" label="连接池大小" max="20" min="0" step="1">
           <template v-slot:append>
             <v-text-field
                 v-model="config.pool_size"
+                :readonly="running"
                 density="compact"
                 hide-details
                 single-line
@@ -107,17 +113,17 @@ export default {
           </template>
         </v-slider>
         <v-row>
-          <v-checkbox v-model="config.enable_ipset" label="全局代理"></v-checkbox>
-          <v-checkbox v-model="config.inverse_route" label="反转地址"></v-checkbox>
+          <v-checkbox v-model="config.enable_ipset" :readonly="running" label="全局代理"></v-checkbox>
+          <v-checkbox v-model="config.inverse_route" :readonly="running" label="反转地址"></v-checkbox>
         </v-row>
         <v-container class="rounded-xl, border">
-          <v-checkbox v-model="config.enable_dns" label="信任DNS"
-                      @click="config.enable_dns=!config.enable_dns"></v-checkbox>
+          <v-checkbox v-model="config.enable_dns" :readonly="running"
+                      label="信任DNS" @click="config.enable_dns=!config.enable_dns"></v-checkbox>
           <div v-if="config.enable_dns">
-            <v-text-field v-model="config.dns_listen" :rules="[check_ipv4]" label="监听地址"
-                          variant="outlined"></v-text-field>
-            <v-text-field v-model="config.trust_dns" :rules="[check_ipv4]" label="可信DNS地址"
-                          variant="outlined"></v-text-field>
+            <v-text-field v-model="config.dns_listen" :readonly="running" :rules="[check_ipv4]"
+                          label="监听地址" variant="outlined"></v-text-field>
+            <v-text-field v-model="config.trust_dns" :readonly="running" :rules="[check_ipv4]"
+                          label="可信DNS地址" variant="outlined"></v-text-field>
           </div>
         </v-container>
         <v-btn :disabled="!is_config_ok()" block color="blue" size="x-large" @click="do_action">{{ label }}</v-btn>
