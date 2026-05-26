142306b169703aa1b1fbbca114aa4b1a41647ef5
hoping <baihaoping@gmail.com>
2026-05-26 13:33:49 +0800

    remove obsolete mobile2 app

--- mobile2/backend/gen/bin/desktop.rs
+++
@@ -1,5 +0,0 @@
-
-fn main() {
-    #[cfg(not(any(target_os = "android", target_os = "ios")))]
-    mobiletrojan::main().unwrap();
-}
--- mobile2/frontend/src/layouts/default/AppBar.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <v-app-bar color="blue" :title="store.title">
-  </v-app-bar>
-</template>
-
-<script lang="ts" setup>
-import {useAppStore} from "@/store/app"
-let store = useAppStore();
-</script>
