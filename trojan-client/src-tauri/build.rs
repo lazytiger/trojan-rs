5fb0e802a5751ad83d6d67d4b78833bc013e3c1e
hoping <baihaoping@gmail.com>
2026-05-27 10:27:41 +0800

    Add macOS trojan-client build workflow

--- trojan-client/src-tauri/build.rs
+++ trojan-client/src-tauri/build.rs
@@ -1,26 +1,29 @@
 fn main() {
     let profile = std::env::var("PROFILE").unwrap();
     let target = std::env::var("TARGET").unwrap();
-    let file = format!("../../target/{}/trojan.exe", profile);
+    let exe = if target.contains("windows") { ".exe" } else { "" };
+    let file = format!("../../target/{}/trojan{}", profile, exe);
     println!("cargo:rerun-if-changed={}", file);
-    let new_file = format!("libs/trojan-{}.exe", target);
+    let new_file = format!("libs/trojan-{}{}", target, exe);
     std::fs::create_dir_all("libs").unwrap();
     std::fs::copy(file, new_file).unwrap();
 
-    let wintun_arch = if target.contains("x86_64") {
-        "amd64"
-    } else if target.contains("i686") {
-        "x86"
-    } else if target.contains("aarch64") {
-        "arm64"
-    } else if target.contains("arm") {
-        "arm"
-    } else {
-        panic!("unsupported target for wintun.dll: {}", target);
-    };
-    let wintun_file = format!("../../wintun/bin/{}/wintun.dll", wintun_arch);
-    println!("cargo:rerun-if-changed={}", wintun_file);
-    std::fs::copy(wintun_file, "libs/wintun.dll").unwrap();
+    if target.contains("windows") {
+        let wintun_arch = if target.contains("x86_64") {
+            "amd64"
+        } else if target.contains("i686") {
+            "x86"
+        } else if target.contains("aarch64") {
+            "arm64"
+        } else if target.contains("arm") {
+            "arm"
+        } else {
+            panic!("unsupported target for wintun.dll: {}", target);
+        };
+        let wintun_file = format!("../../wintun/bin/{}/wintun.dll", wintun_arch);
+        println!("cargo:rerun-if-changed={}", wintun_file);
+        std::fs::copy(wintun_file, "libs/wintun.dll").unwrap();
+    }
 
     let mut windows = tauri_build::WindowsAttributes::new();
     windows = windows.app_manifest(
